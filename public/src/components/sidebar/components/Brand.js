7129340d17a6ab3143e6071d390763ef492a2fe3
AbdelrhmanElswah <abdoelrhmanelsawah@gmail.com>
2024-08-05 11:59:05 +0300

    updates commit

--- public/src/components/sidebar/components/Brand.js
+++ public/src/components/sidebar/components/Brand.js
@@ -1,19 +1,32 @@
 import React from "react";
-
 // Chakra imports
-import { Flex, useColorModeValue } from "@chakra-ui/react";
-
+import { Flex, useColorModeValue, Image, Text } from "@chakra-ui/react";
+// Import the image
+import userAvatar from "assets/img/avatars/avatar1.png";
 // Custom components
-import { HorizonLogo } from "components/icons/Icons";
 import { HSeparator } from "components/separator/Separator";
 
 export function SidebarBrand() {
-  //   Chakra color mode
+  // Chakra color mode
   let logoColor = useColorModeValue("navy.700", "white");
+
+  const user = {
+    profileImage: userAvatar, // User's profile image URL
+    name: "John Doe" // User's name
+  };
 
   return (
     <Flex align='center' direction='column'>
-      <HorizonLogo h='26px' w='175px' my='32px' color={logoColor} />
+      <Image
+        borderRadius='full'
+        boxSize='70px'
+        src={user.profileImage} // Use the imported image
+        alt={user.name}
+        mb='12px'
+      />
+      <Text color={logoColor} fontWeight='bold' mb='32px'>
+        {user.name} 
+      </Text>
       <HSeparator mb='20px' />
     </Flex>
   );
